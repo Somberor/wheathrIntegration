097edbcfcb14125bab95875c87a36a1aad253ffb
somberor <FIRST_NAME LAST_NAME>
2022-10-26 01:49:57 +0300

    Add structing of wheather data of hours

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 // Return api in specific range
 
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
 
 async function main() {
     let data = await getWeather(); // default request for testing.
@@ -11,6 +11,8 @@ async function main() {
     let texasData = await getWeather(31.9686, 99.9018, 7); // get texas wheater for 7 days
     console.log(texasData);
 
+    //
+
 }
 
 
@@ -18,7 +20,16 @@ async function main() {
 async function getWeather(lat = 51.5074, lon = 0.1278, days = 5) {
     let res = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m`);
     let data = await res.json();
-    return data;
+    let dates = data.hourly.time;
+    let temps = data.hourly.temperature_2m;
+    let result = [];
+    for (let i = 0; i < dates.length; i++) {
+        result.push({
+            date: dates[i],
+            temp: temps[i]
+        });
+    }
+    return result;
 }
 
 main();
